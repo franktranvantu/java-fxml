62da13e2de44a8cdee59210c4ea9711606a93350
franktranvantu <59758589+franktranvantu@users.noreply.github.com>
2021-02-26 16:01:13 +0700

    Update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,13 @@
 package sample;
 
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    public Button button;
+
+    public void handleClick() {
+        System.out.println("You clicked");
+        button.setText("Click me again");
+    }
 }
